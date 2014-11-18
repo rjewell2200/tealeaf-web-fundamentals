178125df4dc7002998ea650d3e03fae9fddfd454
Rob Jewell <jewell.robertp@gmail.com>
2014-11-17 20:57:17 -0500

    allow server to return document based on dynamic url

--- http_server.rb
+++ http_server.rb
@@ -6,6 +6,9 @@ puts "HTTP Server ready to accept requests!"
 loop do
   connection = server.accept
   puts "Opening a connection for request:"
+  message_line = connection.gets
+  path = message_line.split(' ')[1]
+  puts path
   while message_line = connection.gets
     puts message_line
     break if message_line.chomp == ""
@@ -16,7 +19,7 @@ loop do
   connection.puts "Content-Type: text/html"
   connection.puts "Server: My Http Server"
   connection.puts
-  connection.puts File.read('documents/hello_world.html')
+  connection.puts File.read("documents/#{path}")
   connection.close
   puts 'Response sent and connection closed.'
 end
